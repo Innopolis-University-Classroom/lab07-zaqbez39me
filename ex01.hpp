ba74b3ddd2673a519061716dcc99239b1f41ad6e
zaqbez39me <korolov_k@mail.ru>
2022-03-07 13:45:55 +0300

    Ex01-fix

--- ex01.hpp
+++ ex01.hpp
@@ -5,5 +5,9 @@
 template <typename T>
 void multiples (T& sum, T x, int n)
 {
-    sum = 1 + x * n * (1 + n) / 2;
+    if (n > 0)
+        sum += 1;
+    for(int i = 1; i <= n; ++i){
+        sum += x * i;
+    }
 }
